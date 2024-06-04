422258dd90e528701eaeee75892f3324364d7319
namanuppal <namanuppal678@gmail.com>
2024-06-04 14:14:03 +0530

    Added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,7 +24,7 @@ export default function Home() {
       <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
         <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
           <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
-          <a href="Resume.pdf" download>
+          <a href="NAMAN UPPAL.pdf" download>
             <h2 className="text-4xl font-bold sm:text-4xl">
               Download Resume
             </h2>
